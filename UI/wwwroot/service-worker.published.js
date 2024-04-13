424bdb734ebb0960154f93d329558a1af7744dea
Joorak <aliakbar.rezapour@gmail.com>
2024-04-13 23:50:24 +0330

    some bug-fix...

--- UI/wwwroot/service-worker.published.js
+++ UI/wwwroot/service-worker.published.js
@@ -6,7 +6,7 @@ self.addEventListener('install', event => event.waitUntil(onInstall(event)));
 self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
 self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
-const cacheNamePrefix = 'offline-cache-0005';
+const cacheNamePrefix = 'offline-cache-0007';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
 const offlineAssetsInclude = [/\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/];
 const offlineAssetsExclude = [/^service-worker\.js$/];
